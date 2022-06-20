697642b40d57d74d36223eae2336b1415718492e
Gustavo Augustini <63506279+gaugustini@users.noreply.github.com>
2022-06-19 21:20:58 -0300

    Refactoring

---
+++ app/src/main/java/com/gaugustini/myexamples/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gaugustini.myexamples
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
--- app/src/main/java/com/gaugustini/myexamples/ui/bottomnavigation/HomeFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.gaugustini.myexamples.ui.bottomnavigation
-
-import androidx.fragment.app.Fragment
-import com.gaugustini.myexamples.R
-
-class HomeFragment : Fragment(R.layout.fragment_home)
